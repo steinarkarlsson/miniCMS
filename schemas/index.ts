5679b4db37ba62f05fd52bbd5a19cfc47db3e6d2
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-15 21:15:34 +1200

    Fix missing Race schema from index

--- schemas/index.ts
+++ schemas/index.ts
@@ -5,4 +5,5 @@ import print from './print'
 import character from './character'
 import faction from './faction'
 import releaseWave from './releaseWave'
-export const schemaTypes = [figure, set, terrain, print, character, faction, releaseWave]
+import race from './race'
+export const schemaTypes = [figure, set, terrain, print, character, faction, releaseWave, race]
---
+++ schemas/race.ts
@@ -0,0 +1,11 @@
+export default {
+  name: 'race',
+  title: 'Race',
+  type: 'document',
+  fields: [
+    {
+      name: 'name',
+      title: 'Name',
+      type: 'string'
+    }
+  ]};
