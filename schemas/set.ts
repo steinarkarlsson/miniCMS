2ed5b0fceb43e1ad0762f6d7d99d7c588a6c6050
Steinar Karlsson <steinarkarlsson@gmail.com>
2024-08-18 08:48:17 +1200

    Added description, alias, figures, terrain and print set schema

--- schemas/set.ts
+++ schemas/set.ts
@@ -7,6 +7,51 @@ export default {
       name: 'mainName',
       title: 'Main Name',
       type: 'string'
-    }
+    },
+    {
+      name: 'figures',
+      title: 'Figures',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {type: 'figure'},
+          ]
+        }
+      ]
+    },
+    {
+      name: 'print',
+      title: 'Print',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {type: 'print'},
+          ]
+        }
+      ]
+    },
+    {
+      name: 'terrain',
+      title: 'Terrain',
+      type: 'array',
+      of: [
+        {
+          type: 'reference',
+          to: [
+            {type: 'terrain'},
+          ]
+        }
+      ]
+    },
+    {
+      name: 'description',
+      title: 'Description',
+      type: 'array',
+      of: [{type: 'block'}]
+    },
   ]
 }
